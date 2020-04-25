41dee455b6cf03d0fdcc8f4b2fdb669087a0f171
hshine1226 <hshine1226@gmail.com>
2020-04-25 20:35:34 +0900

    기능: Canvas에 지정한 색 채우기 기능 추가

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.querySelector("#jsRange");
 const mode = document.querySelector("#jsMode");
-/*캔버스의 크기를 지정해줘야 한다.*/
-canvas.width = 700;
-canvas.height = 700;
 
-ctx.strokeStyle = "#2d3436";
+const INITIAL_COLOR = "#2d3436";
+const CANVAS_SIZE = "700";
+
+let filling = true;
+/*캔버스의 크기를 지정해줘야 한다.*/
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
+
+ctx.fillStyle = INITIAL_COLOR;
 
 let painting = false;
 
@@ -35,7 +42,7 @@ function onMouseMove(event) {
 function handleColorClick(event) {
   const color = event.target.style.backgroundColor;
   ctx.strokeStyle = color;
-  console.log(ctx.strokeStyle);
+  ctx.fillStyle = color;
 }
 
 function handleRangeChange(event) {
@@ -44,10 +51,18 @@ function handleRangeChange(event) {
 }
 
 function handleModeClick(event) {
-  if (mode.innerText == "Draw") {
-    mode.innerText = "Fill";
-  } else {
+  if (filling) {
+    filling = false;
     mode.innerText = "Draw";
+  } else {
+    filling = true;
+    mode.innerText = "Fill";
+  }
+}
+
+function handleCanvasClick() {
+  if (!filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZEr);
   }
 }
 
@@ -56,6 +71,7 @@ if (canvas) {
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
 }
 
 Array.from(colors).forEach((color) =>
